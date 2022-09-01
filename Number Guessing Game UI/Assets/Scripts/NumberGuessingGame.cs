25a0950f7e39c4ed2bd83fe49b92c7be75856d21
Justin Scott <hello@jrs.dev>
2022-09-01 09:51:50 -0700

    UI changes

--- Number Guessing Game UI/Assets/Scripts/NumberGuessingGame.cs
+++ Number Guessing Game UI/Assets/Scripts/NumberGuessingGame.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberGuessingGame : MonoBehaviour
 {
-    [SerializeField] private int guess;
+    private int guess;
     [SerializeField] private int minValue;
     [SerializeField] private int maxValue;
+    [SerializeField] private TextMeshProUGUI guessText;
 
     // Start is called before the first frame update
     void Start()
@@ -14,41 +16,25 @@ public class NumberGuessingGame : MonoBehaviour
         StartGame();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I guessed your number, I'm a genius!");
-            Debug.Log("");
-            StartGame();
-        }
-    }
-
     public void OnPressHigher()
     {
-        minValue = guess;
-        guess = (maxValue + minValue) / 2;
-        Debug.Log("Is it higher or lower than: " + guess);
+        minValue = guess + 1;
+        NextGuess();
     }
 
-    public  void OnPressLower()
+    public void OnPressLower()
     {
-        maxValue = guess;
-        guess = (maxValue + minValue) / 2;
-        Debug.Log("Is it higher or lower than: " + guess);
+        maxValue = guess - 1;
+        NextGuess();
     }
 
     public void NextGuess()
     {
-
+        guess = Random.Range(minValue, maxValue + 1);
+        guessText.text = guess.ToString();
     }
     void StartGame()
     {
-        guess = 5;
-        minValue = 1;
-        maxValue = 10;
-        maxValue = maxValue + 1;
+        NextGuess();
     }
 }
